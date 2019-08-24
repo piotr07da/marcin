7d724f1b09333c52d06e64bfb8462f80cc692c85
MarcinB <marcin_bejger@wp.pl>
2019-08-25 00:15:09 +0200

    ex

---
+++ Zadania/Reloaded/Example.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reloaded
+{
+    public class Example
+    {
+        public void Test()
+        {
+            int x = 0;
+            
+            var stworek = new Stworek(2000);
+            
+            stworek.OdzywSie(new Banan());
+        }
+    }
+
+    class Stworek
+    {
+        private int _energia;
+
+        public Stworek()
+        {
+            _energia = 100;
+        }
+
+        public Stworek(int energiaPoczatkowa)
+        {
+            _energia = energiaPoczatkowa;
+        }
+
+        public void OdzywSie(IZywnosc z)
+        {
+            _energia += z.Kcal();
+        }
+
+    }
+
+    public interface IZywnosc
+    {
+        int Kcal();
+    }
+
+    public class Banan : IZywnosc
+    {
+        public int Kcal()
+        {
+            return 100;
+        }
+    }
+
+    public class Kurczak : IZywnosc
+    {
+        public int Kcal()
+        {
+            return 500;
+        }
+    }
+
+}
