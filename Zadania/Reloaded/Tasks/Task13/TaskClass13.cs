1c16abf2ab905a3f6bcfe7e4f130e53fdec8f024
MarcinB <marcin_bejger@wp.pl>
2019-08-29 01:08:38 +0200

    Task13replay

--- Zadania/Reloaded/Tasks/Task13/TaskClass13.cs
+++ Zadania/Reloaded/Tasks/Task13/TaskClass13.cs
@@ -10,56 +10,47 @@ namespace Reloaded.Tasks.Task13
     public class TaskClass13
 
     {
-        internal Rectangle Test()
+        public Rectangle Test()
         {
             var rectangle = new Rectangle();
             Console.Clear();
-            Console.Write("Wpisz szerokość : ");
-            for (; ; )
-            {
-                bool results = double.TryParse(Console.ReadLine(), out rectangle.width);
 
-                if (results)
-                {
-                    if (rectangle.width <= 0)
-                    {
-                        Console.Write("Wpisz liczbę większą od 0 : ");
-                    }
-                    else
-                    {
-                        break;
-                    }
-                }
-                else
-                {
-                    Console.Write("Wpisz LICZBĘ większą od 0 : ");
-                }
-            }
-            Console.Write("Wpisz wysokość : ");
-            for(; ; )
-            {
-                bool results = double.TryParse(Console.ReadLine(), out rectangle.height);
-                if (results)
-                {
-                    if(rectangle.height <= 0)
-                    {
-                        Console.Write("Wpisz liczbę większą od 0 : ");
-                    }
-                    else
-                    {
-                        break;
-                    }
-                }
-                else
-                {
-                    Console.Write("Wpisz LICZBĘ większą od 0 : ");
-                }
-            }
-            double calculateArea = rectangle.CalculateArea();         //tu coś napewno
-            Console.WriteLine("Pole prostokąta = "+calculateArea);    //napitoliłem
+            rectangle.width = ReadDoubleFromConsole("Wpisz szerokość : ");
+
+            rectangle.height = ReadDoubleFromConsole("Wpisz wysokość : ");
+
+            double calculateArea = rectangle.CalculateArea();
+            Console.WriteLine("Pole prostokąta = " + calculateArea);
             Console.ReadKey();
+
+            double ReadDoubleFromConsole(string enterValueMessage)
+
+            {
+                double value=0;
+                Console.Write(enterValueMessage);
+                for (; ; )
+                {
+                    bool results = double.TryParse(Console.ReadLine(), out value);
+
+                    if (results)
+                    {
+                        if (value <= 0)
+                        {
+                            Console.Write("Wpisz liczbę większą od 0 : ");
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        Console.Write("Wpisz LICZBĘ większą od 0 : ");
+                    }
+                }
+                return value;
+            }
             return rectangle;
-           
         }
     }
 }
