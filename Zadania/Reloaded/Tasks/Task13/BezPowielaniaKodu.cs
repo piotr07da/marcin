0a096be4acfabbce99fbda64fa0e488a8f0d539f
Piotr Bejger <piotr.bejger@gmail.com>
2019-08-28 14:09:11 +0200

    DRY - przykład

---
+++ Zadania/Reloaded/Tasks/Task13/BezPowielaniaKodu.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reloaded.Tasks.Task13
+{
+    public class BezPowielaniaKodu
+    {
+        public void Test()
+        {
+            int x1, x2, x3, x4, x5, x6;
+            
+            x1 = WriteMessageAndReturnMessageLength(1, "wiadomość pierwsza");
+            x2 = WriteMessageAndReturnMessageLength(2, "wiadomość druga");
+            x3 = WriteMessageAndReturnMessageLength(3, "wiadomość trzecia");
+            x4 = WriteMessageAndReturnMessageLength(4, "wiadomość czwarta");
+            x5 = WriteMessageAndReturnMessageLength(5, "wiadomość piąta");
+            x6 = WriteMessageAndReturnMessageLength(6, "wiadomość szósta");
+
+            Console.WriteLine(x1 + x2 + x3 + x4 + x5 + x6);
+        }
+
+        private int WriteMessageAndReturnMessageLength(int messageNumber, string message)
+        {
+            Console.WriteLine(messageNumber + ".");
+            Console.WriteLine("******************");
+            Console.WriteLine(message);
+            Console.WriteLine("##################");
+            return message.Length;
+        }
+    }
+}
---
+++ Zadania/Reloaded/Tasks/Task13/ZPowieleniemKodu.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reloaded.Tasks.Task13
+{
+    public class ZPowieleniemKodu
+    {
+        public void Test()
+        {
+            int x1, x2, x3, x4, x5, x6;
+
+            Console.WriteLine("1.");
+            Console.WriteLine("******************");
+            Console.WriteLine("wiadomość pierwsza");
+            Console.WriteLine("##################");
+            x1 = "wiadomość pierwsza".Length;
+
+            Console.WriteLine("2.");
+            Console.WriteLine("******************");
+            Console.WriteLine("wiadomość druga");
+            Console.WriteLine("##################");
+            x2 = "wiadomość druga".Length;
+
+            Console.WriteLine("3.");
+            Console.WriteLine("******************");
+            Console.WriteLine("wiadomość trzecia");
+            Console.WriteLine("##################");
+            x3 = "wiadomość trzecia".Length;
+
+            Console.WriteLine("4.");
+            Console.WriteLine("******************");
+            Console.WriteLine("wiadomość czwarta");
+            Console.WriteLine("##################");
+            x4 = "wiadomość czwarta".Length;
+
+            Console.WriteLine("5.");
+            Console.WriteLine("******************");
+            Console.WriteLine("wiadomość piąta");
+            Console.WriteLine("##################");
+            x5 = "wiadomość piąta".Length;
+
+            Console.WriteLine("6.");
+            Console.WriteLine("******************");
+            Console.WriteLine("wiadomość szósta");
+            Console.WriteLine("##################");
+            x6 = "wiadomość szósta".Length;
+
+            Console.WriteLine(x1 + x2 + x3 + x4 + x5 + x6);
+        }
+    }
+}
