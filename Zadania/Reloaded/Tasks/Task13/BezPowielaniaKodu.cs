9548d20eacf554a23ab30cb276e05f0edaf4c0a2
MarcinB <marcin_bejger@wp.pl>
2019-08-31 21:39:45 +0200

    ...

--- Zadania/Reloaded/Tasks/Task13/BezPowielaniaKodu.cs
+++ Zadania/Reloaded/Tasks/Task13/BezPowielaniaKodu.cs
@@ -13,7 +13,7 @@ namespace Reloaded.Tasks.Task13
             int x1, x2, x3, x4, x5, x6;
             
             x1 = WriteMessageAndReturnMessageLength(1, "wiadomość pierwsza");
-            x2 = WriteMessageAndReturnMessageLength(2, "wiadomość druga");
+            x2 = WriteMessageAndReturnMessageLength(2, "wiadomość   druga");
             x3 = WriteMessageAndReturnMessageLength(3, "wiadomość trzecia");
             x4 = WriteMessageAndReturnMessageLength(4, "wiadomość czwarta");
             x5 = WriteMessageAndReturnMessageLength(5, "wiadomość piąta");
@@ -25,7 +25,7 @@ namespace Reloaded.Tasks.Task13
         private int WriteMessageAndReturnMessageLength(int messageNumber, string message)
         {
             Console.WriteLine(messageNumber + ".");
-            Console.WriteLine("******************");
+            Console.WriteLine("------------------");
             Console.WriteLine(message);
             Console.WriteLine("##################");
             return message.Length;
