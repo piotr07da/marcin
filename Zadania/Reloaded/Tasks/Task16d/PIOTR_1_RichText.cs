8694fc96a674d2e329fb666b5ca65ebfc6a8f72d
Piotr Bejger <piotr.bejger@gmail.com>
2019-09-08 00:37:54 +0200

    uwagi do 16d

---
+++ Zadania/Reloaded/Tasks/Task16d/PIOTR_1_RichText.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+
+namespace Reloaded.Tasks.Task16d
+{
+    public class PIOTR_1_RichText
+    {
+        public PIOTR_1_RichText(string text, string letterSeparator, LetterCaseType letterCase,  ConsoleColor color, bool underline, bool reverseOrder)
+        {
+            Text = text;
+            LetterSeparator = letterSeparator;
+            LetterCase = letterCase;
+            Color = color;
+            ReverseOrder = reverseOrder;
+            Underline = underline;
+        }
+
+        public string Text { get; set; }
+        public string LetterSeparator { get; set; }
+        public LetterCaseType LetterCase { get; set; }     
+        public bool Underline { get; set; }
+        public bool ReverseOrder { get; set; }
+        public ConsoleColor Color { get; set; }
+
+        public void Show()
+        {
+            var temporaryText = Text;
+
+            Console.ForegroundColor = Color;
+
+            switch (LetterCase)
+            {
+                case LetterCaseType.Lower:
+                    temporaryText = temporaryText.ToLower();
+                    break;
+
+                case LetterCaseType.Upper:
+                    temporaryText = temporaryText.ToUpper();
+                    break;
+            }
+
+            if (ReverseOrder)
+            {
+                var temporaryText2 = string.Empty;
+                for (int i = 0; i < temporaryText.Length; i++)
+                {
+                    temporaryText2 += temporaryText[temporaryText.Length - 1 - i];
+                }
+                temporaryText = temporaryText2;
+            }
+
+            if (!string.IsNullOrEmpty(LetterSeparator))
+            {
+                var temporaryText2 = string.Empty;
+                for (int i = 0; i < temporaryText.Length; i++)
+                {
+                    temporaryText2 += temporaryText[i];
+                    if (i < temporaryText.Length - 1)
+                    {
+                        temporaryText2 += LetterSeparator;
+                    }
+                }
+                temporaryText = temporaryText2;
+            }
+
+            Console.WriteLine(temporaryText);
+            if (Underline)
+            {
+                for (int i = 0; i < temporaryText.Length; i++)
+                {
+                    Console.Write("_");
+                }
+            }
+            Console.ReadKey();
+        }
+
+        public enum LetterCaseType
+        {
+            Original,
+            Upper,
+            Lower,
+        }
+    }
+}
--- Zadania/Reloaded/Tasks/Task16d/RichText.cs
+++ Zadania/Reloaded/Tasks/Task16d/RichText.cs
@@ -39,7 +39,7 @@ namespace Reloaded.Tasks.Task16d
            
             //if (Color==ConsoleColor.Red)
             //{
-                Console.ForegroundColor = Color;
+                Console.ForegroundColor = Color; // [PIOTR] tak - ten if, który zakomentowałeś jest do wywalenia
             //}
             if (LetterCase == LetterCaseType.Upper)
             {
@@ -50,12 +50,17 @@ namespace Reloaded.Tasks.Task16d
                 temporaryText = temporaryText.ToLower();
             }
 
-          if (RewerseOrder == true)
+          if (RewerseOrder == true) // [PIOTR] zamiast pisać: if (ReverseOrder == true) można napisać po prostu: if (ReverseOrder)
             {
-                var tab = temporaryText.ToCharArray();
+                var tab = temporaryText.ToCharArray(); // [PIOTR] tu jest trochę namieszane. tab to Twoja tymczasowa tablica, którą dopiero wypełniasz więc wystarczyło by var tab = new char[temporaryText.Length]; czyli po prostu tablica znaków tego samego rozmiaru co temporaryText
                 var temporaryText2 = "";
                 for (int i = 0; i < temporaryText.Length; i++)
                 {
+                    // [PIOTR] tutaj zamiast przypisywać do tymczasowej tablicy to mógłbyś od razu pisać:
+                    // temporaryText2 = temporaryText2 + temporaryText[temporaryText.Length - 1 - i];
+                    // dzięki temu nie potrzebował byś zupełnie tej drugiej pętli
+                    // no bo teraz to tutaj wpisujesz znaki do tymczasowej tablicy, a w drugiej pętli przepisujesz z tymczasowej tablicy do temporaryText2 - niepotrzebna robota
+
                     tab[i] = temporaryText[temporaryText.Length - 1 - i];
                 }
                
@@ -67,12 +72,13 @@ namespace Reloaded.Tasks.Task16d
                 temporaryText = temporaryText2;
                 
             }
-          if (!(LetterSeparator == null))
+          if (!(LetterSeparator == null)) // [PIOTR] zamiast pisać: if (!(LetterSeparator == null)) można napisać prościej: if (LetterSeparator != null)
             {
                 var tab = temporaryText.ToCharArray();
                 var temporaryText2 = "";
                 for (int i = 0; i < tab.Length ; i++)
                 {
+                    // [PIOTR] poniższą linię wystarczy rozbić w taki sposób żeby w każdym wywołaniu pętli dodawać znak z pierwotnego tekstu, ale separator dodawać po tym znaku tylko wtedy gdy i != tab.Length - 1 --- czyli po ostatni znaku nie dodasz
                     temporaryText2 = temporaryText2 + tab[i] + LetterSeparator;
                 }
                 temporaryText = temporaryText2;
@@ -90,7 +96,7 @@ namespace Reloaded.Tasks.Task16d
 
         }
 
-        public enum LetterCaseType
+        public enum LetterCaseType // [PIOTR] enum lepiej deklarować poza klasą - bezpośrednio w namespace. Dzięki temu jak gdzieś poza klasą się do niego odwołujesz to nie musisz pisać RichText.LetterCaseType tylko bezpośrednio LetterCaseType
         {
             Original,
             Upper,
